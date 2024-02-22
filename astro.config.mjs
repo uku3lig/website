dcfa8b6a244974927c0173d1aedda22fbbe625b2
uku <uku3lig@gmail.com>
2024-02-23 00:43:50 +0100

    initial website implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            class: "heading-link",
+          },
+        },
+      ],
+    ],
+  },
+});
---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { z, defineCollection } from "astro:content";
+
+export const collections = {
+  posts: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+    }),
+  }),
+};
