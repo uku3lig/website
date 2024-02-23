715e11fa5e2d0bf83c93e5f5ae239503b246603e
uku <uku3lig@gmail.com>
2024-02-23 12:15:09 +0100

    fix more stuff: lighthouse edition

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://uku3lig.net",
+  integrations: [sitemap()],
   markdown: {
     rehypePlugins: [
       rehypeSlug,
