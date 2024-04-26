173db032996c7f99a1246f9917be3f3178fb9e4e
uku <hi@uku.moe>
2024-04-26 16:45:39 +0200

    feat: add tags to posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,7 @@ export const collections = {
       title: z.string(),
       date: z.date(),
       description: z.string(),
+      tags: z.array(z.string()),
     }),
   }),
 };
--- src/content/posts/new-website-again.md
+++ src/content/posts/new-website-again.md
@@ -2,6 +2,7 @@
 title: A new website! (again)
 date: 2024-02-23
 description: uku3lig.net 5th edition (never seen before!)
+tags: ["meta"]
 ---
 
 finally! after all this time!
