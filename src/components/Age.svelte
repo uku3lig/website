cf8b14d53cb772fb8d7d4764e6b3e5f847b9d5a7
uku <hi@uku.moe>
2026-03-25 01:08:33 +0100

    feat: add age widget

---
+++ src/components/Age.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { DateTime } from "luxon";
+
+  const birthday = DateTime.fromISO("2005-12-18T00:00:00+01:00");
+  const alive = birthday
+    .diffNow(["year", "month", "day", "hour", "minute", "second"])
+    .negate()
+    .mapUnits(Math.floor);
+</script>
+
+<span id="age" title={alive.toHuman().replaceAll(",", "")}
+  >{Math.floor(alive.as("year"))} year old</span
+>
+
+<style>
+  #age {
+    cursor: help;
+    text-decoration: underline dotted;
+  }
+</style>
